2027c0ba55f37236280b2b08bc3971ed60a9878d
FYYZJH <114062707+FYYZJH@users.noreply.github.com>
2022-09-22 21:49:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -79,6 +79,6 @@ elif temperature<=20:
 else:
   sid="[温度不高不低，但也要注意及时补水哦]"
   
-data = {"weather":{"value":wea},"daytime":{"value":get_weekday()},"temperature":{"value":str(temperature)+"℃","love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"weather":{"value":wea},"daytime":{"value":get_weekday()},"temperature":{"value":str(temperature)+"℃"},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
